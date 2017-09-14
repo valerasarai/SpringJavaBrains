fec4a5268fbbd168017e3d25630004e111d657f6
ValeraJava <valera.sarai@gmail.com>
2017-09-14 18:15:59 +0300

    Example 02 final

--- Example02/src/main/java/com/valerastudy/Example02Main.java
+++ Example02/src/main/java/com/valerastudy/Example02Main.java
@@ -6,15 +6,20 @@ import org.springframework.core.io.FileSystemResource;
 
 public class Example02Main {
     public static void main(String[] args) {
+        drawingWithObjectFactory();
+        drawingWithSpring();
+    }
 
-// for refactoring
-//        Triangle triangle = new Triangle();
-//        triangle.draw();
+    private static void drawingWithObjectFactory() {
+        System.out.println("Instantiate the object triangle using Object Factory: ");
+        Triangle triangle = new Triangle();
+        triangle.draw();
+    }
 
-// for refactoring
+    private static void drawingWithSpring() {
+        System.out.println("Instantiate the object triangle using SPRING amd Beam Factory: ");
         BeanFactory factory = new XmlBeanFactory(new FileSystemResource("Example02//src//main//resources//spring.xml"));
         Triangle triangle = (Triangle) factory.getBean("triangle");
         triangle.draw();
-
     }
 }
