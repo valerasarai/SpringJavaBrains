535bcc855fd54b5d36117684d2d0590a76b5e037
ValeraJava <valera.sarai@gmail.com>
2017-09-14 18:00:59 +0300

    Example 02 but need refactoring

--- Example02/src/main/java/com/valerastudy/Example02Main.java
+++ Example02/src/main/java/com/valerastudy/Example02Main.java
@@ -1,4 +1,20 @@
 package com.valerastudy;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 public class Example02Main {
+    public static void main(String[] args) {
+
+// for refactoring
+//        Triangle triangle = new Triangle();
+//        triangle.draw();
+
+// for refactoring
+        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("Example02//src//main//resources//spring.xml"));
+        Triangle triangle = (Triangle) factory.getBean("triangle");
+        triangle.draw();
+
+    }
 }
---
+++ Example02/src/main/java/com/valerastudy/Triangle.java
@@ -0,0 +1,7 @@
+package com.valerastudy;
+
+public class Triangle {
+    public void draw() {
+        System.out.println("This is a drow method from TRIANGLE!");;
+    }
+}
